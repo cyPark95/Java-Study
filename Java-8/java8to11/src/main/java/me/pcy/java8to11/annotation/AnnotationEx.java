33b2f02eaeca2e8dc51033d3baa5976cee1a48f5
pcy <cas15987@gmail.com>
2022-11-30 16:29:39 +0900

    어노테이션의 변화

---
+++ Java-8/java8to11/src/main/java/me/pcy/java8to11/annotation/AnnotationEx.java
@@ -0,0 +1,29 @@
+package me.pcy.java8to11.annotation;
+
+import java.lang.annotation.ElementType;
+import java.util.Arrays;
+
+@Chicken("양념")
+@Chicken("간장")
+public class AnnotationEx {
+
+    public static void main(String[] args) {
+        // 어노테이션으로 조회
+        Chicken[] chickens = AnnotationEx.class.getAnnotationsByType(Chicken.class);
+        Arrays.stream(chickens).forEach(System.out::println);
+
+        // 어노테이션 컨테이너로 조회
+        ChickenContainer chickenContainer = AnnotationEx.class.getAnnotation(ChickenContainer.class);
+        Arrays.stream(chickenContainer.value()).forEach(System.out::println);
+    }
+
+    // ElementType.TYPE_PARAMETER 제네릭 타입에만 사용
+    // ElementType.TYPE_USE 타입을 선언한 모든곳에 사용
+    static class FellsLikeChicken<@Chicken("제네릭") T> {
+
+        public static <C> void print(@Chicken("타입") C c) {
+            System.out.println(c);
+        }
+    }
+
+}
