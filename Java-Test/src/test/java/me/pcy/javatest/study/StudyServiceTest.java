206c87f0a4ec26bdae5a81d6f9b0c031be68a33f
pcy <cas15987@gmail.com>
2022-12-16 14:40:07 +0900

    BDD 스타일 Mockito API

--- Java-Test/src/test/java/me/pcy/javatest/study/StudyServiceTest.java
+++ Java-Test/src/test/java/me/pcy/javatest/study/StudyServiceTest.java
@@ -14,6 +14,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -140,5 +142,31 @@ class StudyServiceTest {
 
         // 호출될께 없어야 한다.
         verifyNoMoreInteractions(memberService);
+    }
+
+    @Test
+    void BDDMockito() {
+        // Given
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("pcy@email.com");
+
+        Study study = new Study(10, "테스트");
+
+        given(memberService.findById(1L)).willReturn(Optional.of(member));
+        given(studyRepository.save(study)).willReturn(study);
+
+        // When
+        studyService.createNewStudy(1L, study);
+
+        // Then
+        assertEquals(member, study.getOwner());
+
+        // 1번 호출되어야 한다.
+        then(memberService).should(times(1)).notify(study);
+        then(memberService).should(times(1)).notify(member);
+
+        // 호출될께 없어야 한다.
+        then(memberService).shouldHaveNoMoreInteractions();
     }
 }
