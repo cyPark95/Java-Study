26c54bd2da35a07961d99c4c39324feb2a2e5b07
pcy <cas15987@gmail.com>
2022-12-19 15:11:07 +0900

    Testcontainer 로깅

--- Java-Test/Docker-Test/src/test/java/me/pcy/dockertest/study/StudyServiceTest.java
+++ Java-Test/Docker-Test/src/test/java/me/pcy/dockertest/study/StudyServiceTest.java
@@ -1,8 +1,10 @@
 package me.pcy.dockertest.study;
 
+import lombok.extern.slf4j.Slf4j;
 import me.pcy.dockertest.domain.Member;
 import me.pcy.dockertest.domain.Study;
 import me.pcy.dockertest.member.MemberService;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,7 +13,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -25,24 +32,32 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 
+@Slf4j
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 @Testcontainers
+@ContextConfiguration(initializers = StudyServiceTest.ContainerPropertyInitializer.class)
 class StudyServiceTest {
 
     @Mock
     MemberService memberService;
     @Autowired
     StudyRepository studyRepository;
+    @Autowired
+    Environment environment;
 
     @Container
     static GenericContainer postgresSQLContainer = new GenericContainer("postgres")
             .withExposedPorts(5432)
+            .withEnv("POSTGRES_DB", "studytest")
             .withEnv("POSTGRES_PASSWORD", "studytest");
 
     @BeforeEach
     void beforeEach() {
+        System.out.println(postgresSQLContainer.getLogs());
+        System.out.println(environment.getProperty("container.port"));
+
         studyRepository.deleteAll();
     }
 
@@ -84,5 +99,14 @@ class StudyServiceTest {
         assertEquals(OPENED, study.getStatus());
         assertNotNull(study.getOpenedDateTime());
         then(memberService).should().notify(study);
+    }
+
+    static class ContainerPropertyInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
+            TestPropertyValues.of("container.port=" + postgresSQLContainer.getMappedPort(5432))
+                    .applyTo(applicationContext.getEnvironment());
+        }
     }
 }
