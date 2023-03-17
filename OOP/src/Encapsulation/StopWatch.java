57885866616643da9047a8902b7ae9cc3be44faf
cyPark95 <cas15987@gmail.com>
2023-03-17 09:46:39 +0900

    절차지향 -> 객체지향

--- OOP/src/Encapsulation/StopWatch.java
+++ OOP/src/Encapsulation/StopWatch.java
@@ -1,21 +1,42 @@
 package Encapsulation;
 
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
 public class StopWatch {
 
     public void time() {
         Timer t = new Timer();
-        t.startTime = System.currentTimeMillis();
+        t.start();
 
         // ...
 
-        t.stopTime = System.currentTimeMillis();
+        t.stop();
 
-        long elaspedTime = t.stopTime - t.startTime;
+        long time = t.elaspedTime(MILLISECONDS);
     }
 
     private static class Timer {
         public long startTime;
         public long stopTime;
+
+        public void start() {
+            startTime = System.currentTimeMillis();
+        }
+
+        public void stop() {
+            stopTime = System.currentTimeMillis();
+        }
+
+        public long elaspedTime(TimeUnit unit) {
+            switch (unit) {
+                case MILLISECONDS:
+                    return stopTime - startTime;
+                default:
+                    return 0;
+            }
+        }
     }
 
 }
