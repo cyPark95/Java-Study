ce071fc78330cff6611ccc364c1d24aa15ffbe0c
cyPark95 <cas15987@gmail.com>
2023-03-17 09:37:09 +0900

    Refactoring
    - 데이터를 들고있는 쪽에 기능을 구현
    - 구현하면서 필요한 데이터를 파라미터로 받도록

--- OOP/src/Encapsulation/Rental.java
+++ OOP/src/Encapsulation/Rental.java
@@ -6,16 +6,20 @@ public class Rental {
     private int daysRented;
 
     public int getFrequentRenterPoints() {
-        if (movie.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
-            return 2;
-        else
-            return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 
     private static class Movie {
         public static int REGULAR = 0;
         public static int NEW_RELEASE = 1;
         private int priceCode;
+
+        public int getFrequentRenterPoints(int daysRented) {
+            if (priceCode == Movie.NEW_RELEASE && daysRented > 1)
+                return 2;
+            else
+                return 1;
+        }
 
         public int getPriceCode() {
             return priceCode;
